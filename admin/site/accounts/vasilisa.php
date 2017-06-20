124caab44ed1c0121bf3ba5eae24dfa6e852797a
vasilisa <igorevna.vasilisa@gmail.com>
2017-06-20 08:06:59 +0300

    added new user for the admin panel

---
+++ admin/site/accounts/vasilisa.php
@@ -0,0 +1,12 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: vasilisa
+firstname: Vasilisa
+lastname: Karelova
+email: igorevna.vasilisa@gmail.com
+password: >
+  $2a$10$Td3BoU9JvZqVlCW0gLPpTupB/NCYelxEX7BqS5Oyz..XXXkSaZ3rm
+language: en
+role: admin
+history:
+  - home
